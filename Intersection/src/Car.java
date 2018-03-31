5c3b0c3cad0471e9f45ff773b99cfbf7bd0f621c
Matthew Stockert <mjstocke@umich.edu>
2018-03-31 10:21:48 -0400

    point locks
    Added return in directions class to return the points needed to cross
    the intersection

--- Intersection/src/Car.java
+++ Intersection/src/Car.java
@@ -7,19 +7,17 @@ public class Car {
 	    CrossIntersection(dir);
 	    ExitIntersection(dir);
 	}
-
+	private void ArriveIntersection(Directions dir) {
+		// TODO Auto-generated method stub
+		
+	}
+	private void CrossIntersection(Directions dir) {
+		// TODO Auto-generated method stub
+		
+	}
 	private void ExitIntersection(Directions dir) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	private void CrossIntersection(Directions dir) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private void ArriveIntersection(Directions dir) {
-		// TODO Auto-generated method stub
-		
-	}
 }
